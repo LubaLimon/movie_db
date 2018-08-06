abbdac29b834622a685b349b9bfb68190967be5a
Lubneuski Aliaksandr <lubneuskialiaksandr@localhost.localdomain>
2018-08-06 23:20:51 +0300

    Added search, rating system

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :movies do
+    collection do
+      get 'search'
+    end
+  end	  
+  root 'movies#index'
 end
---
+++ db/migrate/20180806163159_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
